3a8d5c26d02d8d19b23ecd0e7a2aabc069a6f851
dimonkyk <dimashedko2009@gmail.com>
2020-12-14 17:25:10 +0300

    2машины

--- gonki.py
+++ gonki.py
@@ -3,10 +3,16 @@ from  pygame import display, event, image, transform, draw
 okno = display.set_mode([1500,800])
 #подготовка модели
 speed = 0
+povorot2 = 0
 povorot = 0
 trass = pygame.Rect(170,150,1200,500)
 car_r = pygame.Rect(0,0,100,50)
+car2r = pygame.Rect(0,50,90,40)
 #подгатовка к рисование
+car2 = image.load("kartinki/22.png")
+car2 = transform.scale(car2,[100,50])
+car2 = transform.flip(car2,True,True)
+car2.set_colorkey([255,255,255])
 
 car = image.load("kartinki/car1.png")
 car = transform.scale(car,[100,50])
@@ -27,7 +33,12 @@ while True:
     else:
         speed = 1
 
-
+    if case[pygame.K_w]:
+        car2r.x +=2
+    if case[pygame.K_d]==1:
+        povorot2 += -1
+    if case[pygame.K_a]:
+        povorot2 +=1
     if case[pygame.K_RIGHT]==1:
         povorot += -1
     if case[pygame.K_LEFT]==1:
@@ -41,9 +52,11 @@ while True:
     if -315<= povorot <=-225:
             car_r.y -= speed
 
-
+    povorot2=povorot2%-360
     povorot=povorot%-360
     car_pov = transform.rotate(car, povorot)
+    car_pov2 = transform.rotate(car2,povorot2)
+
     car_r.w = car_pov.get_width()
     car_r.h = car_pov.get_height()
     if car_r.colliderect(trass):
@@ -54,7 +67,7 @@ while True:
     #РИСУЕМ КАДР
     okno.fill([0, 0, 0])
     draw.rect(okno,[255,255,255],trass)
-
+    okno.blit(car_pov2,car2r.topleft)
     okno.blit(car_pov,[car_r.x,car_r.y])
     draw.rect(okno,[184,45,205],car_r,5)
     display.flip()
